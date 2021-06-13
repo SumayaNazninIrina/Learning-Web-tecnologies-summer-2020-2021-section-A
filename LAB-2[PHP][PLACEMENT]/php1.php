172a4c6247b79b3d52e6c1a8a21309ac4cb915b3
SumayaNazninIrina <sumayanazninirina@gmail.com>
2021-06-13 22:31:59 +0600

    php1

---
+++ LAB-2[PHP][PLACEMENT]/php1.php
@@ -0,0 +1,9 @@
+<?php
+    $length= 10;
+    $width=16;
+    $Rectangle=$length*$width;
+    $p=$length + $width;
+    $perimeter= 2*$p;
+    echo "Rectangle= $Rectangle <br>";
+    echo "perimeter=$perimeter"
+?>
